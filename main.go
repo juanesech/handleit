ceda444091a3bd92099f093f447d9a69c35483b9
juanesech <juanesech@gmail.com>
2022-08-18 18:18:50 +0200

    feat(main): init

--- main.go
+++ main.go
@@ -1,17 +1,40 @@
-package hello
+package main
 
 import (
 	"fmt"
 	"net/http"
 
-	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/gin-gonic/gin"
 )
 
-func init() {
-	functions.HTTP("HelloGet", helloGet)
+var database []Module
+
+type Module struct {
+	Name      string     `json:"name"`
+	Version   string     `json:"version"`
+	Variables []variable `json:"variables"`
 }
 
-// helloGet is an HTTP Cloud Function.
-func helloGet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, World!")
+type variable struct {
+	Name     string `json:"name"`
+	VarType  string `json:"type"`
+	Required bool   `json:"required"`
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/modules", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, database)
+	})
+
+	router.POST("/modules", func(ctx *gin.Context) {
+		var module Module
+		ctx.BindJSON(&module)
+		database = append(database, module)
+		message := fmt.Sprintf("Module %s posted", module.Name)
+		ctx.String(http.StatusOK, message)
+	})
+
+	router.Run()
 }
