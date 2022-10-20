61d938bc3ab40b2eb0fe8f8bede62ca2292e8420
juanesech <juanesech@gmail.com>
2022-10-20 19:59:55 +0200

    feat(front): [WIP] settings list and details

--- front/src/routes/settings/[source]/index.tsx
+++ front/src/routes/settings/[source]/index.tsx
@@ -30,32 +30,58 @@ export const onGet: RequestHandler<ModuleSource> = async ({ params }) => {
     }
 };
 export default component$(() => {
-  const source = useEndpoint<ModuleSource>();
+    const source = useEndpoint<ModuleSource>();
 
-  return (
-    <Resource
-      value={source}
-      onPending={() => <div>Loading...</div>}
-      onRejected={() => <div>Error</div>}
-        onResolved={(source) => (
-        <>
-        <div class="block content-center mx-auto max-w-xl">
-            <div class="hidden items-center justify-start md:flex md:flex-1 lg:w-0">
-                <a href="#" class="ml-0 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-blue-500 bg-white px-4 py-2 text-base font-medium text-blue shadow-sm hover:text-white hover:bg-blue-500">Modify</a>
-            </div>
-            <div class="m-2 mx-auto rounded-sm p-4 bg-white ring-1">
-                <ul>
-                    <li>{source.ID}</li>
-                    <li>{source.name}</li>
-                    <li>{source.type}</li>
-                    <li>{source.address}</li>
-                    <li>{source.group}</li>
-                    <li>{source.auth}</li>
-                </ul>
-            </div>
-        </div>
-        </>
-      )}
-    />
-  );
+    return (
+        <Resource
+            value={source}
+            onPending={() => <div>Loading...</div>}
+            onRejected={() => <div>Error</div>}
+            onResolved={(source) => (
+                <>
+                    <details>
+                        <summary class="bg-gray-100 group cursor-pointer hover:bg-blue-600 hover:ring-blue-600 list-none flex flex-wrap items-center rounded-sm">
+                            <div class="rounded-sm px-4 py-3 sm:px-6 group-hover:bg-blue-600 hover:ring-blue-600">
+                                <h4 class="group-hover:text-white text-md text-gray-600">{source.name}</h4>
+                                <span class="align-sub text-xs font-medium inline-block py-1 px-2 rounded group-hover:text-semibold group-hover:text-blue-500 bg-blue-100 lowercase last:mr-0 mr-1">
+                                    {source.type}
+                                </span>
+                            </div>
+                            <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+                                <a href="#" class="mx-3 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-blue-500 bg-white px-4 py-2 text-base font-light text-blue shadow-sm hover:text-white hover:bg-blue-500">Modify</a>
+                            </div>
+                        </summary>
+                        <dl>
+                            <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt class="text-xs font-medium text-gray-500">Type</dt>
+                                <dd class="mt-1 text-xs text-gray-900 sm:col-span-2 sm:mt-0">{source.type}</dd>
+                            </div>
+                            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt class="text-xs font-medium text-gray-500">Address</dt>
+                                <dd class="mt-1 text-xs text-gray-900 sm:col-span-2 sm:mt-0">{source.address}</dd>
+                            </div>
+                            {source.type === "GitLab" ?
+                                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt class="text-xs font-medium text-gray-500">Group</dt>
+                                    <dd class="mt-1 text-xs text-gray-900 sm:col-span-2 sm:mt-0">{source.group}</dd>
+                                </div> : <></>}
+                        </dl>
+                    </details>
+                    <div class="block content-center mx-auto max-w-xl">
+
+                        <div class="m-2 mx-auto rounded-sm p-4 bg-white ring-1">
+                            <ul>
+                                <li>{source.ID}</li>
+                                <li>{source.name}</li>
+                                <li>{source.type}</li>
+                                <li>{source.address}</li>
+                                <li>{source.group}</li>
+                                <li>{source.auth}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </>
+            )}
+        />
+    );
 });
