fbce0404127fe842c5fc9c5263fa192fae72933b
juanesech <juanesech@gmail.com>
2022-11-05 12:42:30 +0100

    feat(settings): [WIP] settings editor

--- front/src/routes/layout.tsx
+++ front/src/routes/layout.tsx
@@ -7,7 +7,7 @@ export default component$(() => {
       <main>
         <Header />
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          <div class="px-4 py-6 sm:px-0">
+          <div class="block content-center mx-auto max-w-xl">
             <Slot/>
           </div>
         </div>
--- front/src/routes/settings/[source]/index.tsx
+++ front/src/routes/settings/[source]/index.tsx
@@ -31,6 +31,7 @@ export const onGet: RequestHandler<ModuleSource> = async ({ params }) => {
 };
 export default component$(() => {
     const source = useEndpoint<ModuleSource>();
+    const store = useStore({ editMode: false })
 
     return (
         <Resource
@@ -38,49 +39,38 @@ export default component$(() => {
             onPending={() => <div>Loading...</div>}
             onRejected={() => <div>Error</div>}
             onResolved={(source) => (
-                <>
-                    <details>
-                        <summary class="bg-gray-100 group cursor-pointer hover:bg-blue-600 hover:ring-blue-600 list-none flex flex-wrap items-center rounded-sm">
-                            <div class="rounded-sm px-4 py-3 sm:px-6 group-hover:bg-blue-600 hover:ring-blue-600">
-                                <h4 class="group-hover:text-white text-md text-gray-600">{source.name}</h4>
-                                <span class="align-sub text-xs font-medium inline-block py-1 px-2 rounded group-hover:text-semibold group-hover:text-blue-500 bg-blue-100 lowercase last:mr-0 mr-1">
-                                    {source.type}
-                                </span>
-                            </div>
-                            <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-                                <a href="#" class="mx-3 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-blue-500 bg-white px-4 py-2 text-base font-light text-blue shadow-sm hover:text-white hover:bg-blue-500">Modify</a>
-                            </div>
-                        </summary>
+                <div class="block content-center mx-auto max-w-xl">
+                        <div class="bg-blue-600 ring-blue-600 list-none flex flex-wrap items-center rounded-sm">
+                        <div class="rounded-sm px-4 py-3 sm:px-6 group-hover:bg-blue-600 hover:ring-blue-600">
+                            <h4 class="text-white text-md">{source.name}</h4>
+                            <span class="align-sub text-sm font-medium inline-block py-1 px-2 rounded group-hover:text-semibold group-hover:text-blue-500 bg-blue-100 lowercase last:mr-0 mr-1">
+                                {source.type}
+                            </span>
+                        </div>
+                        <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+                            <a href="#" class="mx-3 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-blue-500 bg-white px-4 py-2 text-base font-light text-blue shadow-sm hover:text-white hover:bg-blue-500">Modify</a>
+                        </div>
+                        </div>
                         <dl>
+                        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt class="text-md font-medium text-gray-500">Id</dt>
+                                <dd class="mt-1 text-md text-gray-900 sm:col-span-2 sm:mt-0">{source.ID}</dd>
+                            </div>
                             <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt class="text-xs font-medium text-gray-500">Type</dt>
-                                <dd class="mt-1 text-xs text-gray-900 sm:col-span-2 sm:mt-0">{source.type}</dd>
+                                <dt class="text-md font-medium text-gray-500">Type</dt>
+                                <dd class="mt-1 text-md text-gray-900 sm:col-span-2 sm:mt-0">{source.type}</dd>
                             </div>
                             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt class="text-xs font-medium text-gray-500">Address</dt>
-                                <dd class="mt-1 text-xs text-gray-900 sm:col-span-2 sm:mt-0">{source.address}</dd>
+                                <dt class="text-md font-medium text-gray-500">Address</dt>
+                                <dd class="mt-1 text-md text-gray-900 sm:col-span-2 sm:mt-0">{source.address}</dd>
                             </div>
                             {source.type === "GitLab" ?
                                 <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                    <dt class="text-xs font-medium text-gray-500">Group</dt>
-                                    <dd class="mt-1 text-xs text-gray-900 sm:col-span-2 sm:mt-0">{source.group}</dd>
+                                    <dt class="text-md font-medium text-gray-500">Group</dt>
+                                    <dd class="mt-1 text-md text-gray-900 sm:col-span-2 sm:mt-0">{source.group}</dd>
                                 </div> : <></>}
                         </dl>
-                    </details>
-                    <div class="block content-center mx-auto max-w-xl">
-
-                        <div class="m-2 mx-auto rounded-sm p-4 bg-white ring-1">
-                            <ul>
-                                <li>{source.ID}</li>
-                                <li>{source.name}</li>
-                                <li>{source.type}</li>
-                                <li>{source.address}</li>
-                                <li>{source.group}</li>
-                                <li>{source.auth}</li>
-                            </ul>
-                        </div>
-                    </div>
-                </>
+                </div>
             )}
         />
     );
